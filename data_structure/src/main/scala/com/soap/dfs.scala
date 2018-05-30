639bf97295fb837708b5301bef20d25c2b904c83
Unknown <yangfuzhao@meituan.com>
2018-05-30 23:03:40 +0800

    深度搜索

---
+++ data_structure/src/main/scala/com/soap/dfs.scala
@@ -0,0 +1,30 @@
+package com.soap
+
+object dfs {
+
+  val a : Array[Int] = new Array[Int](10)
+  /**
+    * 标记
+    */
+  val book = new Array[Int](10)
+
+  val n : Int = 0
+
+  def genDFS(step: Int) {
+    if (step == n+1){
+      a.foreach(print(_))
+      return
+    }
+
+    var i = 0
+    for (i <- 1 to n){
+      if(book[i] == 0){}
+    }
+
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
